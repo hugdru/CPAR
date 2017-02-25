6ad5220954c36544f1c97cd5e5b0b1f0d423a3e8
Hugo Drumond <hugo.drumond@fe.up.pt>
2017-02-25 14:50:15 +0000

    golang time print change

--- projeto1/golang/src/matrixprod/matrix.go
+++ projeto1/golang/src/matrixprod/matrix.go
@@ -58,7 +58,7 @@ func MultiplicationNaiveSequential(matrixA, matrixB *Matrix) *Matrix {
 		}
 	}
 	elapsed := time.Since(start)
-	fmt.Printf("Time: %3.3f\n", elapsed.Seconds())
+	fmt.Printf("%3.3f\n", elapsed.Seconds())
 
 	return matrixResult
 }
