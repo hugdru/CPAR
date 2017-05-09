8ee18e645320eb19c53d356afa845804adb133bd
Pedro Miguel Herdeiro Vaz De Moura <up201306843@B02AU1043.fe.up.pt>
2017-05-09 17:35:32 +0100

    update xD

--- projeto2/benchmarks.sh
+++ projeto2/benchmarks.sh
@@ -4,20 +4,20 @@ set -e
 
 cd "${0%/*}"
 
-#echo "**** Benchmarking i-sequential ****"
-#./i-sequential/benchmark.sh
+echo "**** Benchmarking i-sequential ****"
+./i-sequential/benchmark.sh
 
-#echo "**** Benchmarking i-sequential-improved ****"
-#./i-sequential-improved/benchmark.sh
+echo "**** Benchmarking i-sequential-improved ****"
+./i-sequential-improved/benchmark.sh
 
 echo -e "\n\n**** Benchmarking ii-openmp ****"
 ./ii-openmp/benchmark.sh
 
-#echo -e "\n\n**** Benchmarking ii-openmp-improved ****"
-#./ii-openmp-improved/benchmark.sh
+echo -e "\n\n**** Benchmarking ii-openmp-improved ****"
+./ii-openmp-improved/benchmark.sh
 
 echo -e "\n\n**** Benchmarking iii-mpi ****"
 ./iii-mpi/benchmark.sh
 
-#echo -e "\n\n**** Benchmarking iv-mpiOpenmp ****"
-#./iv-mpiOpenmp/benchmark.sh
+echo -e "\n\n**** Benchmarking iv-mpiOpenmp ****"
+./iv-mpiOpenmp/benchmark.sh
